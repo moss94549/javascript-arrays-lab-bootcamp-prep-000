f67621c5dedc93b6a3d086b2ec32ba428b97eb87
moss94549@gmail.com <moss94549@gmail.com>
2018-04-29 06:05:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-const app = "I don't do much."
+const app = "I don't do much";
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendKitten(name)
+{
+  kitten.push(name);
+  return;
+}
+
+function destructivelyPrependKitten(name)
+{
+  kitten.unshift(name);
+  return kitten;
+}
+
+function destructivelyRemoveLastKitten(name)
+{
+  kitten.pop(name);
+  return kitten;
+}
+
+function destructivelyRemoveFirstKitten(name)
+{
+  kitten.shift(name);
+  return kitten;
+}
+
+function appendKitten(name)
+{
+  return [...kitten,name];
+}
+
+function prependKitten(name)
+{
+ return [name, ...kitten]; 
+}
+
+function removeLastKitten(name)
+{
+  return kitten.slice(0, kitten.length - 1 );
+}
+
+function removeFirstKitten(name)
+{
+  return kitten.slice(1);
+}
